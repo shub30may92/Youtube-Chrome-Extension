46d78359ede146ceeab8da18e2cd8d5a8cc71c56
Shubham Gupta <shub30mayjugal@gmaoil.com>
2016-02-21 12:39:54 +0530

    version 2.1, added on click action

--- js/popup.js
+++ js/popup.js
@@ -9,15 +9,15 @@
  *   is found.
  */
 function random_func(){
-  // console.log("called");
-  // var TabId = 0;
-  // chrome.tabs.query({'url':'*://www.youtube.com/watch*'},function(tab){
-  //     // console.log(tab[0].url);
-  //     TabId = tab[0].id;
-  //     chrome.tabs.executeScript(TabId,{
-  //       code: "document.querySelector('.ytp-play-button').click();"
-  //     });
-  // });
+  console.log("called");
+  var TabId = 0;
+  chrome.tabs.query({'url':'*://www.youtube.com/watch*'},function(tab){
+      // console.log(tab[0].url);
+      TabId = tab[0].id;
+      chrome.tabs.executeScript(TabId,{
+        code: "document.querySelector('.ytp-play-button').click();"
+      });
+  });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
