0173ec0e183445894c5d3efde312cdc688799be2
Shubham Gupta <shub30mayjugal@gmaoil.com>
2016-03-08 17:50:02 +0530

    support for gaana, but needs teting

--- js/background.js
+++ js/background.js
@@ -7,22 +7,45 @@ chrome.commands.onCommand.addListener(function(command) {
   console.log('onCommand event received for message: ', command);
   var TabId = 0;
   chrome.tabs.query({'url':'*://www.youtube.com/watch*'},function(tab){
-      // console.log(tab[0].url);
-      TabId = tab[0].id;
-      if(command == "play_pause"){
-	      chrome.tabs.executeScript(TabId,{
-	        code: "document.querySelector('.ytp-play-button').click();"
-	      });
-	  }
-	  if(command == "play_next"){
-	      chrome.tabs.executeScript(TabId,{
-	        code: "document.querySelector('.ytp-next-button').click();"
-	      });
-	  }
-	  if(command == "play_previous"){
-	      chrome.tabs.executeScript(TabId,{
-	        code: "document.querySelector('.ytp-prev-button').click();"
-	      });
-	  }
-  });
+      if(tab != undefined){
+	      TabId = tab[0].id;
+	      if(command == "play_pause"){
+		      chrome.tabs.executeScript(TabId,{
+		        code: "document.querySelector('.ytp-play-button').click();"
+		      });
+		  }
+		  if(command == "play_next"){
+		      chrome.tabs.executeScript(TabId,{
+		        code: "document.querySelector('.ytp-next-button').click();"
+		      });
+		  }
+		  if(command == "play_previous"){
+		      chrome.tabs.executeScript(TabId,{
+		        code: "document.querySelector('.ytp-prev-button').click();"
+		      });
+		  }
+		}
+		else{
+			chrome.tabs.query({'url':'*://gaana.com/*'},function(tab){
+			if(tab != undefined){
+			  TabId = tab[0].id;
+			  if(command == "play_pause"){
+			      chrome.tabs.executeScript(TabId,{
+			        code: "document.querySelector('.playPause').click();"
+			      });
+			  }
+			  if(command == "play_next"){
+			      chrome.tabs.executeScript(TabId,{
+			        code: "document.querySelector('.next').click();"
+			      });
+			  }
+			  if(command == "play_previous"){
+			      chrome.tabs.executeScript(TabId,{
+			        code: "document.querySelector('.prev').click();"
+			      });
+			  }
+			}
+		});
+
+  	}
 });
--- js/popup.js
+++ js/popup.js
@@ -9,17 +9,43 @@
  *   is found.
  */
 function random_func(){
-  console.log("called");
-  var TabId = 0;
-  chrome.tabs.query({'url':'*://www.youtube.com/watch*'},function(tab){
-      // console.log(tab[0].url);
-      TabId = tab[0].id;
-      chrome.tabs.executeScript(TabId,{
-        code: "document.querySelector('.ytp-play-button').click();"
-      });
-  });
-}
+	
+	var other_tab_found = false;
+	chrome.tabs.query({'url' : '*://gaana.com/*'},function(tab){
+		if(tab.length>0){
+			other_tab_found = true;
+			// console.log('1'+other_tab_found);
+			var TabId = 0;
+			TabId = tab[0].id;
+			chrome.tabs.executeScript(TabId,{
+			code: "document.querySelector('.playPause').click();"
+			});
+			// console.log('2'+other_tab_found);
+		}
+		else{
+			other_tab_found = false;		//if gaana and saavan tabs not found, go for youtube
+		}
+	});
+	chrome.tabs.query({'url' : '*://www.youtube.com/watch*'},function(tab){
+		if(tab.length>0){
+			TabId = tab[0].id;
 
+			if(other_tab_found){
+				if(tab[0].audible){
+					chrome.tabs.executeScript(TabId,{
+						code: "document.querySelector('.ytp-play-button').click();"
+					});
+				}
+			}
+			else{
+				// console.log('4'+other_tab_found);
+				chrome.tabs.executeScript(TabId,{
+				code: "document.querySelector('.ytp-play-button').click();"
+				});
+			}
+		}
+	});
+}
 document.addEventListener('DOMContentLoaded', function() {
-  random_func();
+	random_func();
 });
